cd8eb6a9b068ca23ac93f117b318bc0fddcb7bd0
aqil2514 <muhamadaqil383@gmail.com>
2023-11-04 15:09:56 +0700

    first commit

---
+++ src/Test.jsx
@@ -0,0 +1,9 @@
+import { useCallback } from 'react';
+
+export default function ProductPage({ productId, referrer, theme }) {
+  const handleSubmit = useCallback((orderDetails) => {
+    post('/product/' + productId + '/buy', {
+      referrer,
+      orderDetails,
+    });
+  }, [productId, referrer]);
